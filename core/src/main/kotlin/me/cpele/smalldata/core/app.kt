3c51c773ebd36d80d6302c7cd51e6207180b7fcf
christoufair <geekarist@users.noreply.github.com>
2024-04-08 18:55:16 +0200

    Rename Obsidian-related model attributes

--- core/src/main/kotlin/me/cpele/smalldata/core/app.kt
+++ core/src/main/kotlin/me/cpele/smalldata/core/app.kt
@@ -10,8 +10,8 @@ object App {
     data class Model(
         val query: String? = null,
         val results: List<String>? = null,
-        val backend: Obsidian.Details? = null,
-        val searchJob: Job? = null,
+        val obsidianBackend: Obsidian.Details? = null,
+        val obsidianSearchJob: Job? = null,
     ) {
 
         companion object
@@ -28,13 +28,15 @@ object App {
 
         data class QueryChanged(val query: String) : Event
 
-        data class ReceivedResults(val results: List<Obsidian.Finding>) : Event
+        sealed interface ObsidianSearch : Event {
+            data class AuthReceived(val auth: Obsidian.Details) : ObsidianSearch
 
-        data class AuthReceived(val auth: Obsidian.Details) : Event
+            data class Launched(val job: Job) : ObsidianSearch
 
-        data class SearchLaunched(val job: Job) : Event
+            data class ReceivedResults(val results: List<Obsidian.Finding>) : ObsidianSearch
 
-        data class OpenFileRequested(val path: String) : Event
+            data class OpenFileRequested(val path: String) : ObsidianSearch
+        }
     }
 
     data class Context(val obsidian: Obsidian, val logger: Logger)
@@ -53,19 +55,20 @@ fun App.view(model: App.Model, dispatch: (App.Event) -> Unit): App.View = run {
         }
 
     val authUim =
-        if (model.backend?.authenticated == true) {
+        if (model.obsidianBackend?.authenticated == true) {
             listOf(
-                UiModel.TextLabel("Status: ${model.backend.status}"),
-                UiModel.TextLabel("Obsidian: ${model.backend.versions.obsidian}"),
-                UiModel.TextLabel("REST API: ${model.backend.versions.self}"),
+                UiModel.TextLabel("Status: ${model.obsidianBackend.status}"),
+                UiModel.TextLabel("Obsidian: ${model.obsidianBackend.versions.obsidian}"),
+                UiModel.TextLabel("REST API: ${model.obsidianBackend.versions.self}"),
             )
         } else {
             listOf(UiModel.Button("Authenticate") { dispatch(App.Event.AuthRequested) })
         }
 
     val resultsUim =
-        model.results?.map { UiModel.Button(it) { dispatch(App.Event.OpenFileRequested(it)) } }
-            ?: emptyList()
+        model.results?.map {
+            UiModel.Button(it) { dispatch(App.Event.ObsidianSearch.OpenFileRequested(it)) }
+        } ?: emptyList()
     App.View(queryUim, authUim, resultsUim)
 }
 
@@ -75,28 +78,32 @@ fun App.update(model: App.Model, event: App.Event): Change<App.Model, App.Event>
         App.Event.AuthRequested ->
             Change(model) { dispatch ->
                 val auth = obsidian.auth()
-                dispatch(App.Event.AuthReceived(auth))
+                dispatch(App.Event.ObsidianSearch.AuthReceived(auth))
             }
-        is App.Event.AuthReceived -> Change(model = model.copy(backend = event.auth))
-        is App.Event.QueryChanged ->
-            Change(model.copy(query = event.query)) { dispatch ->
-                logger.info("Canceling prior search job")
-                model.searchJob?.cancel()
-                val job = launch {
-                    logger.info("Query-changed job was launched")
-                    logger.info("Searching notes for query: ${event.query}")
-                    val results: List<Obsidian.Finding> = obsidian.notes(event.query)
-                    logger.info("Found ${results.size} results: $results")
-                    val receivedResults: App.Event = App.Event.ReceivedResults(results)
-                    logger.info("\"Debouncing\" for 1 sec")
-                    delay(1.seconds) // Kind of debounce
-                    logger.info("Dispatching event: $receivedResults")
-                    dispatch(receivedResults)
-                }
-                dispatch(App.Event.SearchLaunched(job = job))
-            }
-        is App.Event.ReceivedResults ->
+        is App.Event.QueryChanged -> updateOnQueryChanged(model, event)
+        is App.Event.ObsidianSearch.AuthReceived ->
+            Change(model = model.copy(obsidianBackend = event.auth))
+        is App.Event.ObsidianSearch.Launched -> Change(model.copy(obsidianSearchJob = event.job))
+        is App.Event.ObsidianSearch.ReceivedResults ->
             Change(model.copy(results = event.results.map { finding -> finding.label }))
-        is App.Event.SearchLaunched -> Change(model.copy(searchJob = event.job))
-        is App.Event.OpenFileRequested -> Change(model) { obsidian.open(event.path) }
+        is App.Event.ObsidianSearch.OpenFileRequested -> Change(model) { obsidian.open(event.path) }
+    }
+
+private fun App.Context.updateOnQueryChanged(model: App.Model, event: App.Event.QueryChanged) =
+    Change(model.copy(query = event.query)) { dispatch ->
+        logger.info("Canceling prior Obsidian search job")
+        model.obsidianSearchJob?.cancel()
+        val obsidianJob = launch {
+            logger.info("Query-changed job was launched")
+            logger.info("Searching notes for query: ${event.query}")
+            val results: List<Obsidian.Finding> = obsidian.notes(event.query)
+            logger.info("Found ${results.size} results: $results")
+            val receivedObsidianResults: App.Event =
+                App.Event.ObsidianSearch.ReceivedResults(results)
+            logger.info("\"Debouncing\" for 1 sec")
+            delay(1.seconds) // Kind of debounce
+            logger.info("Dispatching event: $receivedObsidianResults")
+            dispatch(receivedObsidianResults)
+        }
+        dispatch(App.Event.ObsidianSearch.Launched(job = obsidianJob))
     }
