2e2e22ac65327931df2b70eed7e2fd62fef247d2
christoufair <geekarist@users.noreply.github.com>
2024-05-06 18:36:39 +0200

    Set API key and engine ID

--- shell/src/main/kotlin/me/cpele/smalldata/shell/RestGoogleEffects.kt
+++ shell/src/main/kotlin/me/cpele/smalldata/shell/RestGoogleEffects.kt
@@ -1,12 +1,10 @@
 package me.cpele.smalldata.shell
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.http.GenericUrl
 import com.google.api.client.http.HttpRequestInitializer
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.customsearch.v1.CustomSearchAPI
 import me.cpele.smalldata.core.GoogleEffects
-import java.net.URL
 
 class RestGoogleEffects : GoogleEffects {
     private val apiKey = System.getenv("GOOGLE_API_KEY")
@@ -22,14 +20,10 @@ class RestGoogleEffects : GoogleEffects {
     override suspend fun search(query: String): List<GoogleEffects.Findings> {
         val transport = GoogleNetHttpTransport.newTrustedTransport()
         val jsonFactory = GsonFactory.builder().build()
-        val reqInit = HttpRequestInitializer { req ->
-            val url = req.url.toURL()
-            val authenticatedUrl = URL(url, "?apiKey=$apiKey")
-            req.url = GenericUrl(authenticatedUrl)
-        }
+        val reqInit = null
         val api = CustomSearchAPI.Builder(transport, jsonFactory, reqInit).build()
         val engine = api.cse()
-        val response = engine.list().setQ(query).execute()
+        val response = engine.list().setCx("7535d0d5996bf4a3c").setKey(apiKey).setQ(query).execute()
         val items = response.items
         return items.map { GoogleEffects.Findings(label = it.title) }
     }
